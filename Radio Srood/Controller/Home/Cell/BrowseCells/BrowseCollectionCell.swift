421dfeaa08d0679d9af5fc746431a3bef3e2bb37
Hardik <hardik@gmail.com>
2026-06-17 10:20:49 +0530

    set circle for the featured artist

--- Radio Srood/Controller/Home/Cell/BrowseCells/BrowseCollectionCell.swift
+++ Radio Srood/Controller/Home/Cell/BrowseCells/BrowseCollectionCell.swift
@@ -12,9 +12,14 @@ class BrowseCollectionCell: UICollectionViewCell {
     @IBOutlet private weak var itemImage: UIImageView!
     @IBOutlet private weak var lblTitle: UILabel!
     @IBOutlet private weak var lblSubtitle: UILabel!
+
+    private var imageHeightConstraint: NSLayoutConstraint?
+    private var usesCircularImage = false
+    private let defaultImageHeight: CGFloat = 180
     
     var playlist: Playlist? {
         didSet {
+            usesCircularImage = false
             if let playlist = playlist {
                 if let url = URL(string: playlist.playlistCover) {
                     itemImage.af_setImage(withURL: url, placeholderImage: UIImage(named: "Lav_Radio_Logo.png"))
@@ -27,6 +32,7 @@ class BrowseCollectionCell: UICollectionViewCell {
     
     var newRelease: NewRelease? {
         didSet {
+            usesCircularImage = false
             if let newRelease = newRelease {
                 if let url = URL(string: newRelease.newReleasesCover) {
                     itemImage.af_setImage(withURL: url, placeholderImage: UIImage(named: "Lav_Radio_Logo.png"))
@@ -39,6 +45,7 @@ class BrowseCollectionCell: UICollectionViewCell {
 
     var featuredBrowsePlaylist: BrowseFeaturedPlaylist? {
         didSet {
+            usesCircularImage = false
             if let featuredBrowsePlaylist = featuredBrowsePlaylist {
                 if let url = URL(string: featuredBrowsePlaylist.coverURL) {
                     itemImage.af_setImage(withURL: url, placeholderImage: UIImage(named: "Lav_Radio_Logo.png"))
@@ -52,7 +59,9 @@ class BrowseCollectionCell: UICollectionViewCell {
 
     var similarArtist: SimilarArtist? {
         didSet {
+            usesCircularImage = similarArtist != nil
             if let similarArtist = similarArtist {
+                itemImage.contentMode = .scaleAspectFill
                 if let url = URL(string: similarArtist.artistPhoto) {
                     itemImage.af_setImage(withURL: url, placeholderImage: UIImage(named: "Lav_Radio_Logo.png"))
                 } else {
@@ -73,10 +82,14 @@ class BrowseCollectionCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        imageHeightConstraint = itemImage.constraints.first { $0.firstAttribute == .height }
     }
 
     override func prepareForReuse() {
         super.prepareForReuse()
+        usesCircularImage = false
+        itemImage.contentMode = .scaleAspectFit
+        imageHeightConstraint?.constant = defaultImageHeight
         playlist = nil
         newRelease = nil
         featuredBrowsePlaylist = nil
@@ -88,7 +101,15 @@ class BrowseCollectionCell: UICollectionViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        itemImage.layer.cornerRadius = 4
         itemImage.clipsToBounds = true
+
+        if usesCircularImage {
+            let diameter = itemImage.bounds.width
+            imageHeightConstraint?.constant = diameter
+            itemImage.layer.cornerRadius = diameter / 2
+        } else {
+            imageHeightConstraint?.constant = defaultImageHeight
+            itemImage.layer.cornerRadius = 4
+        }
     }
 }
